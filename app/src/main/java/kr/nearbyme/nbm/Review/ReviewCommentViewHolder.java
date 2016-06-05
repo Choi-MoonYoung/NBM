74e6d5f7022cb0798947054c4b05e51d303da81f
Choi-MoonYoung <cmy35492820@gmail.com>
2016-06-05 18:23:57 +0900

    first design

--- app/src/main/java/kr/nearbyme/nbm/Review/ReviewCommentViewHolder.java
+++ app/src/main/java/kr/nearbyme/nbm/Review/ReviewCommentViewHolder.java
@@ -11,14 +11,13 @@ import kr.nearbyme.nbm.data.Comment;
  * Created by CHOIMOONYOUNG on 2016. 5. 19..
  */
 public class ReviewCommentViewHolder extends RecyclerView.ViewHolder{
-    TextView cmtNameView, cmtContentView, cmtDateView;
+    TextView cmtNameView, cmtContentView;
     Comment mData;
 
     public ReviewCommentViewHolder(View itemView) {
         super(itemView);
         cmtNameView = (TextView) itemView.findViewById(R.id.text_username);
         cmtContentView = (TextView) itemView.findViewById(R.id.text_usercomment);
-        cmtDateView = (TextView) itemView.findViewById(R.id.text_date);
 
         }
 
@@ -27,7 +26,6 @@ public class ReviewCommentViewHolder extends RecyclerView.ViewHolder{
         mData = data;
         cmtNameView.setText(data.getCmt_writerName());
         cmtContentView.setText(data.getCmt_content());
-        cmtDateView.setText(data.getCmt_regDate());
 
 
     }
