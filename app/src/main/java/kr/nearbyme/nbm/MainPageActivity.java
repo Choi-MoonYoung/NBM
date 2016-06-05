0b321d13b20dbdf46c46693925d3f8a3150be82a
Choi-MoonYoung <cmy35492820@gmail.com>
2016-06-05 18:58:17 +0900

    design

--- app/src/main/java/kr/nearbyme/nbm/MainPageActivity.java
+++ app/src/main/java/kr/nearbyme/nbm/MainPageActivity.java
@@ -1,25 +1,23 @@
 package kr.nearbyme.nbm;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import kr.nearbyme.nbm.MainActivity;
-
 public class MainPageActivity extends AppCompatActivity {
-    Button btnKakao, btnFacebook;
+    Button btnEnter, btnFacebook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
-        btnKakao = (Button) findViewById(R.id.btn_kakao);
+        btnEnter = (Button) findViewById(R.id.btn_enter);
         btnFacebook = (Button) findViewById(R.id.btn_facebook);
 
-        btnKakao.setOnClickListener(new View.OnClickListener() {
+        btnEnter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainPageActivity.this, MainActivity.class);
